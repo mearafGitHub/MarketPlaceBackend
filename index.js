e0a20a1fdd6e56d81d7b0d6b07ff027c94fe2739
mearaf <mearaftadewos@gmail.com>
2023-12-12 00:19:29 +0300

    Routing methods skeleton -- work inporgress

--- index.js
+++ index.js
@@ -7,3 +7,49 @@ app.listen(
     () => console.log(`Server running on http://localhost:${port}`)
 )
 
+app.get("/auctionNfts", function (request, result) {
+
+    const sample = {}
+    res.send(sample);
+});
+
+app.get('/fixedPriceNfts', async function(req, res, next){
+    const sample = {}
+    res.send(sample);
+});
+
+app.post("/creatNFT", function (request, result) {
+    
+    const sample = {}
+    res.send(sample);
+});
+
+app.post('/createAuction', async function(req, res, next){
+    const sample = {}
+    res.send(sample);
+});
+
+app.get('/getAuctionEndTime', async function(req, res, next){
+    const sample = {}
+    res.send(sample);
+});
+
+app.get('/bidders', async function(req, res, next){
+    const sample = {}
+    res.send(sample);
+});
+
+app.patch('/startAuction', async function(req, res, next){
+    const sample = {}
+    res.send(sample);
+});
+
+app.post('/endAuction', async function(req, res, next){
+    const sample = {}
+    res.send(sample);
+});
+
+app.get('/withdrawCollateral', async function(req, res, next){
+    const sample = {}
+    res.send(sample);
+});
